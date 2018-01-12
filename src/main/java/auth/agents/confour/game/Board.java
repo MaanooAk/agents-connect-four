099f2f51c6da004309b47e085fd331e4c8742441
MaanooAk <akritasak@gmail.com>
2018-01-12 12:15:26 +0200

    Board: Added the connect four checks

--- src/main/java/auth/agents/confour/game/Board.java
+++ src/main/java/auth/agents/confour/game/Board.java
@@ -10,32 +10,52 @@ package auth.agents.confour.game;
  *
  *
  */
-public class Board {
+public final class Board {
 
     // board sizes
     private static final int W = 7;
     private static final int H = 6;
 
+    // board cells
     public static final char E = ' '; // empty
     public static final char P = 'P'; // player
     public static final char O = 'O'; // opponent
 
+    // winner state
+    public static final char N = 'N'; // none
+    public static final char D = 'D'; // draw
+
     private final char[][] cells;
+    private int left;
+
+    private char winner;
 
     public Board() {
 
         cells = new char[W][H];
+
+        clear();
+    }
+
+    public void clear() {
 
         for (int x = 0; x < W; x++) {
             for (int y = 0; y < H; y++) {
                 cells[x][y] = E;
             }
         }
+        left = W*H;
+        winner = N;
+
     }
 
     public void add(int x, char who) {
+        int y = empty(x);
 
-        cells[x][empty(x)] = who;
+        cells[x][y] = who;
+        left -= 1;
+
+        check(x, y);
     }
 
     public char get(int x, int y) {
@@ -49,5 +69,54 @@ public class Board {
         while (cells[x][y] != E) y += 1;
 
         return y;
+    }
+
+    private void check(int x, int y) {
+        char c = get(x, y);
+
+        if (check(c, x, y)) {
+            winner = c;
+        } else if (left == 0) {
+            winner = D;
+        }
+
+    }
+
+    private boolean check(char c, int x, int y) {
+        return check(c, x, y, 0, 1) ||
+                check(c, x, y, 1, 1) ||
+                check(c, x, y, 1, 0);
+    }
+
+    private boolean check(char c, int x, int y, int dx, int dy) {
+        int count = 0;
+
+        for (int i = -4; i <= 4; i++) {
+
+            int ix = x + i*dx;
+            int iy = y + i*dy;
+
+            if (!inside(ix, iy)) continue;
+
+            if (get(ix, iy) != c) {
+                count = 0;
+            } else {
+                count += 1;
+            }
+        }
+
+        return count >= 4;
+    }
+
+    private boolean inside(int x, int y) {
+        return 0 <= x && x < W && 0 <= y && y < H;
+    }
+
+    public boolean hasWinner() {
+        return winner != N;
+    }
+
+    public char getWinner() {
+        return winner;
     }
 }
